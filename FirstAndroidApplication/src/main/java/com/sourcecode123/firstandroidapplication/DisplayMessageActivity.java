2df3ad67414597e2886d4679f9646f7d8db6e313
Aditya Muttur <amuttur123@gmail.com>
2013-09-15 18:34:56 +0530

    updated main activity

---
+++ FirstAndroidApplication/src/main/java/com/sourcecode123/firstandroidapplication/DisplayMessageActivity.java
@@ -0,0 +1,13 @@
+package com.sourcecode123.firstandroidapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Adianu on 15/09/13.
+ */
+public class DisplayMessageActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- FirstAndroidApplication/src/main/java/com/sourcecode123/firstandroidapplication/MainActivity.java
+++ FirstAndroidApplication/src/main/java/com/sourcecode123/firstandroidapplication/MainActivity.java
@@ -3,8 +3,14 @@ package com.sourcecode123.firstandroidapplication;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
 
 public class MainActivity extends Activity {
+
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +25,12 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 }
